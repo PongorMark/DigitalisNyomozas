a9526e416d3983481a7ef2c2deb8a38ae20addc0
PongorMárk <pongor.mark3@gmail.com>
2026-01-29 15:17:23 +0100

    Sikertelen teszt

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            
+            Szemely s1 = new Szemely("Nate Higgerson",67, "Látta a tettest lelőni egy ártatlan feketét");
+			Console.WriteLine(s1);
         }
     }
 }
