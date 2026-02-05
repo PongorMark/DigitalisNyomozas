e305c7ea0ff137b922321eefc42131a9e972a2fd
PongorMárk <pongor.mark3@gmail.com>
2026-02-05 15:05:54 +0100

    Ugy.cs-ben lista megoldása

--- Program.cs
+++ Program.cs
@@ -6,11 +6,15 @@
         {
 			User u1 = new User("Gipsz Jakab", 1, "Nyomozó");
 			Szemely s1 = new Szemely("Trab Antal",67, "szemtanú");
-            Ugy U1 = new Ugy(1, "Louvre ékszerrablás", "3 láthatósági mellénybe öltözött karbantartónak álcázott tolvaj ellopott sok felbecsülhetetlen értékű ékszert, miközben a múzeum NYITVA volt", "folyamatban");
+			Szemely s2 = new Szemely("Kiss Félix", 26, "tolvaj");
+			Bizonyitek b1 = new Bizonyitek(1, "fotó", "A szemtanú fényképeket készített az ékszerek hűlt helyéről.", 5);
+			Bizonyitek b2 = new Bizonyitek(2, "fotó", "A szemtanú lefényképezte, ahogy a gyanúsítottak elektromos rollerekkel versenyeztek.", 3);
+			List<Szemely> szemelyek = new List<Szemely> { s1, s2 };
+			List<Bizonyitek> bizonyitekok = new List<Bizonyitek> { b1, b2 };
+			Ugy U1 = new Ugy(1, "Louvre ékszerrablás", "3 láthatósági mellénybe öltözött karbantartónak álcázott tolvaj ellopott sok felbecsülhetetlen értékű ékszert, miközben a múzeum NYITVA volt", "folyamatban", szemelyek, bizonyitekok);
             UgyAllapot UA1 = new UgyAllapot("lezárt");
             Gyanusitott g1 = new Gyanusitott("Nagy Árpád", 30, "szabad");
             Tanu t1 = new Tanu("Zsíros B. Ödön", "Látta a tetteseket, ahogy megkezdték a rablást.", "2025. 10. 25.");
-            Bizonyitek b1 = new Bizonyitek(1, "fotó", "A szemtanú fényképeket készített az ékszerek hűlt helyéről.", 5);
             Idovonal i1 = new Idovonal("2025. 10. 19. , 9:30", "A rablás kezdete");
 			Idovonal i2 = new Idovonal("2025. 10. 19. , 9:37", "A rablás vége");
 			Idovonal i3 = new Idovonal("2025. 10. 19. , 9:38", "Menekülés elektromos rollerekkel");
--- Ugy.cs
+++ Ugy.cs
@@ -12,23 +12,29 @@ namespace DigitalisNyomozas
 		private string cim;
 		private string leiras;
 		private string allapot;
+		private List<Szemely> szemelyeklistaja;
+		private List<Bizonyitek> bizonyitekoklistaja;
 
-		public Ugy(int id, string cim, string leiras, string allapot)
+		public Ugy(int id, string cim, string leiras, string allapot, List<Szemely> szemelyeklistaja, List<Bizonyitek> bizonyitekoklistaja)
 		{
 			this.id = id;
 			this.cim = cim;
 			this.leiras = leiras;
 			this.allapot = allapot;
+			this.szemelyeklistaja = szemelyeklistaja;
+			this.bizonyitekoklistaja = bizonyitekoklistaja;
 		}
 
 		public int Id { get => id; set => id = value; }
 		public string Cim { get => cim; set => cim = value; }
 		public string Leiras { get => leiras; set => leiras = value; }
 		public string Allapot { get => allapot; set => allapot = value; }
+		internal List<Szemely> Szemelyeklistaja { get => szemelyeklistaja; set => szemelyeklistaja = value; }
+		internal List<Bizonyitek> Bizonyitekoklistaja { get => bizonyitekoklistaja; set => bizonyitekoklistaja = value; }
 
 		public override string ToString()
 		{
-			return $"{this.cim}: {this.id}, {this.Leiras}, {this.allapot}";
+			return $"{this.cim}: {this.id}, {this.Leiras}, {this.allapot}, {string.Join(", ", szemelyeklistaja)}, {string.Join(", ", bizonyitekoklistaja)}";
 		}
 	}
 }
